4a34b7d2bb633d98fb43317ec7d51fb167a25ea1
Denis <denis2000.kuzik@gmail.com>
2023-06-29 17:30:51 +0300

    wip

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "testmodel.h"
 
 // 1) Добавить модель из C++ в qml:
 // https://doc.qt.io/qt-5/qtquick-modelviewsdata-cppmodels.html
 // https://habr.com/ru/articles/302428/
+// https://scythe-studio.com/en/blog/how-to-integrate-qml-and-c-expose-object-and-register-c-class-to-qml
 // 2) Добавить выбор чата через модель
 // 3) Добавить отображение чата
 
@@ -15,6 +18,11 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    // expose C++ object to Qml:
+    TestModel *testModel = new TestModel(&app);
+    engine.rootContext()->setContextProperty("testModel", testModel);
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
---
+++ testmodel.h
@@ -0,0 +1,33 @@
+#ifndef TESTMODEL_H
+#define TESTMODEL_H
+
+#include "qobjectdefs.h"
+#include <QAbstractListModel>
+#include <QStringList>
+
+class TestModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    enum Roles {
+        ColorRole = Qt::UserRole + 1,
+        TextRole
+    };
+
+    TestModel(QObject *parent = 0);
+
+    virtual int rowCount(const QModelIndex &parent) const override;
+    virtual QVariant data(const QModelIndex &index, int role) const override;
+    virtual QHash<int, QByteArray> roleNames() const override;
+    virtual bool setData(const QModelIndex &index, const QVariant &value, int role);
+    virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+
+    Q_INVOKABLE void add();
+
+private:
+    QStringList m_data;
+};
+
+
+#endif // TESTMODEL_H
